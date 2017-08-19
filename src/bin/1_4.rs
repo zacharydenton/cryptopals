8b57e790d754937a974953998fcc63ce41948389
Zach Denton <z@chdenton.com>
2017-08-19 10:20:55 -0700

    Add solution for Set 1, Challenge 4.

---
+++ src/bin/1_4.rs
@@ -0,0 +1,25 @@
+extern crate cryptopals;
+
+use std::io::BufRead;
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut max_score: f32 = 0.;
+    let mut max_decrypted: Vec<u8> = vec![0; 0];
+    for line in stdin.lock().lines() {
+        let string = line.expect("Failed to read input.");
+        let bytes = cryptopals::htob(string.into_bytes());
+        let mut decrypted: Vec<u8> = vec![0; bytes.len()];
+        for c in ('0' as u8)..(127 as u8) {
+            for i in 0..bytes.len() {
+                decrypted[i] = bytes[i] ^ c;
+            }
+            let score = cryptopals::english_score(decrypted.clone());
+            if score > max_score {
+                max_score = score;
+                max_decrypted = decrypted.clone();
+            }
+        }
+    }
+    println!("{}", String::from_utf8(max_decrypted).unwrap());
+}

a245ef260230f7ca3c36077b71c7b6c978720ad6
Zach Denton <z@chdenton.com>
2017-08-18 19:31:59 -0700

    Add solution for Set 1, Challenge 2.

---
+++ src/bin/1_2.rs
@@ -0,0 +1,21 @@
+extern crate cryptopals;
+
+use std::io::{self, Read, Write};
+use std::env::args;
+
+fn main() {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let string1: String = args.next()
+        .expect("Failed to read input.");
+    let string2: String = args.next()
+        .expect("Failed to read input.");
+
+    let bytes1 = cryptopals::htob(string1.into_bytes());
+    let bytes2 = cryptopals::htob(string2.into_bytes());
+    let result = cryptopals::xor(bytes1, bytes2);
+    let hex = cryptopals::btoh(result);
+
+    io::stdout().write(&hex)
+        .expect("Failed to write output.");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 pub fn htob(hex: Vec<u8>) -> Vec<u8> {
     let mut bytes: Vec<u8> = Vec::new();
     for pair in hex.chunks(2) {
-        let a = hval(pair[0]);
-        let b = hval(pair[1]);
+        let a = h2b(pair[0]);
+        let b = h2b(pair[1]);
         bytes.push((a << 4) + b);
     }
     return bytes;
+}
+
+pub fn btoh(bytes: Vec<u8>) -> Vec<u8> {
+    let mut hex: Vec<u8> = Vec::new();
+    for byte in bytes {
+        hex.push(b2h(byte >> 4));
+        hex.push(b2h(byte & 0xf));
+    }
+    return hex;
+}
+
+pub fn xor(vec1: Vec<u8>, vec2: Vec<u8>) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+    for (a, b) in vec1.iter().zip(vec2.iter()) {
+        result.push(a ^ b);
+    }
+    return result;
 }
 
 pub fn btoa(bytes: Vec<u8>) -> Vec<u8> {
@@ -31,11 +48,19 @@ pub fn btoa(bytes: Vec<u8>) -> Vec<u8> {
     return base64;
 }
 
-fn hval(h: u8) -> u8 {
+fn h2b(h: u8) -> u8 {
     return match h as char {
         '0'...'9' => h - ('0' as u8),
         'a'...'f' => 10 + h - ('a' as u8),
         'A'...'F' => 10 + h - ('A' as u8),
         _ => panic!("{} is not a valid hexadecimal character.", h)
     };
+}
+
+fn b2h(b: u8) -> u8 {
+    return match b {
+        0...9 => ('0' as u8) + b,
+        10...15 => ('a' as u8) + (b - 10),
+        _ => panic!("{} is out of hexadecimal range.", b)
+    }
 }
